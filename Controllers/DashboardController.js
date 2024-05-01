9f6340ad39080eb413426347e25c831095575dba
Sumit Kumar <sumitkumar84390@gmail.com>
2024-05-01 07:48:26 +0530

    added try catch in controllers

--- Controllers/DashboardController.js
+++ Controllers/DashboardController.js
@@ -1,18 +1,22 @@
 const fileS = require("fs");
 // { userId: '2113001', password: 'Sumit@S123' }
 const dashboardController = (req, res) => {
-  console.log(req)
-  const loginDetails = req.query;
-  const year = loginDetails.userId[0] + loginDetails.userId[1];
-  const branch = loginDetails.userId[2] + loginDetails.userId[3];
-  const session = year + (parseInt(year) + 1) + branch;
-  fileS.readFile(
-    "./DataBase/Students/StudentData.json",
-    "utf-8",
-    (err, result) => {
-      res.send(JSON.parse(result)[session][loginDetails.userId]);
-    }
-  );
+  try {
+    console.log(req);
+    const loginDetails = req.query;
+    const year = loginDetails.userId[0] + loginDetails.userId[1];
+    const branch = loginDetails.userId[2] + loginDetails.userId[3];
+    const session = year + (parseInt(year) + 1) + branch;
+    fileS.readFile(
+      "./DataBase/Students/StudentData.json",
+      "utf-8",
+      (err, result) => {
+        res.send(JSON.parse(result)[session][loginDetails.userId]);
+      }
+    );
+  } catch (error) {
+    res.send("Internal Server Error");
+  }
 };
 
 module.exports = dashboardController;
--- Controllers/LoginController.js
+++ Controllers/LoginController.js
@@ -1,19 +1,22 @@
 const fileS = require("fs");
 
-
 const loginController = (req, res, user) => {
-  const loginDetails = req.body;
-  let auth = undefined;
-  fileS.readFile("./DataBase/Authantication.json", "utf-8", (err, result) => {
-    auth = JSON.parse(result)[user][loginDetails.userId];
-    if (auth && auth.password === loginDetails.password) {
-      res.send({ userId: loginDetails.userId, login: true, role: auth.role });
-    } else if (auth && auth.password != loginDetails.password) {
-      res.send({ login: false, message: "Incorrect password" });
-    } else {
-      res.send({ login: false, message: "Incorrect user Id and Password" });
-    }
-  });
+  try {
+    const loginDetails = req.body;
+    let auth = undefined;
+    fileS.readFile("./DataBase/Authantication.json", "utf-8", (err, result) => {
+      auth = JSON.parse(result)[user][loginDetails.userId];
+      if (auth && auth.password === loginDetails.password) {
+        res.send({ userId: loginDetails.userId, login: true, role: auth.role });
+      } else if (auth && auth.password != loginDetails.password) {
+        res.send({ login: false, message: "Incorrect password" });
+      } else {
+        res.send({ login: false, message: "Incorrect user Id and Password" });
+      }
+    });
+  } catch (error) {
+    res.send("Internal Server Error");
+  }
 };
 
 module.exports = { loginController };
